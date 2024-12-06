32fa4f06983bd65c8f154aff3d483a6d45bb8923
MohamCoder <mohammedmus1000@gmail.com>
2024-12-06 03:20:45 +0300

    test

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,8 +1,10 @@
-'use client'
 export default function Button() {
   return (
     <div>
-      <span className="text-3xl" onClick={() => console.log("clicked")}>Click me</span>
+      <div className="
+        bg-red-500 content-center text-center w-32 h-12 transition-all inline-block
+        origin-center rounded-2xl hover:bg-blue-500 hover:scale-90 hover:cursor-pointer
+        " >Click me</div>
     </div>
   );
 }
